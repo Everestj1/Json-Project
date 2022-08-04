ce3e871b451323500253fdff6f817486ed870582
E.J <abc@gmail.com>
2022-08-04 12:56:36 +0600

    Json Project

---
+++ index-featured.js
@@ -0,0 +1,37 @@
+fetch('Featured.json').
+then(response => response.json()).
+then(json => {
+    html = "";
+    for(item of json){
+        html += `
+            <div class="card col-3 g-4 border-0" style="height:350px;">
+            <img class="card-img-top" src="${item.img}" alt="Card image" style="width:100%; height:150px">
+            <div class="card-body">
+              <h4 class="card-title">${item.item}</h4>
+              <p class="card-text text-truncate">${item.desc}</p>
+              <a href="#" class="btn btn-primary text-truncate">View More</a>
+            </div>
+            </div>
+            `;
+    }
+    document.getElementById("featured").innerHTML = html;
+});
+
+fetch('Products.json').
+then(response => response.json()).
+then(json => {
+    html1 = "";
+    for(item of json){
+        html1 += `
+            <div class="card col-3 g-4 border-0" style="height:300px;">
+            <img class="card-img-top" src="${item.img}" alt="Card image" style="width:100%; height:150px">
+            <div class="card-body">
+              <h4 class="card-title">${item.item}</h4>
+              <p class="card-text text-truncate">${item.desc}</p>
+              <a href="#" class="btn btn-primary text-truncate">View More</a>
+            </div>
+            </div>
+            `;
+    }
+    document.getElementById("products").innerHTML = html1;
+});
